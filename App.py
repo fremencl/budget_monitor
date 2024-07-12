135024693be5e6f762f948598bef4efa32148495
Cristian <128819507+fremencl@users.noreply.github.com>
2024-07-12 11:48:52 -0400

    Update App.py

--- App.py
+++ App.py
@@ -196,7 +196,7 @@ else:
 data0_incomplete = data0[(data0['Proceso'].isna()) & (data0['Recinto'].isna())].copy()  # Crear una copia explícita
 
 # Generar el enlace de descarga para las filas eliminadas
-csv_pre_merge_data = convertir_a_csv(data0_incomplete)
+csv_filtered_data = convertir_a_csv(data0_incomplete)
 
 # Agregar un botón de descarga en la aplicación
 st.download_button(
